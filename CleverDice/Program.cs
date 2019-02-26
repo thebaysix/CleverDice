bd76c1361b3699197911b57bcfa645b817d73d57
Luke Whitestone <thebay6@gmail.com>
2019-02-25 18:58:03 -0800

    Add solution

---
+++ CleverDice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleverDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome();
+
+            Console.ReadLine();
+
+            Console.WriteLine("EOP");
+        }
+
+        private static void Welcome()
+        {
+            Console.Write("Welcome to ");
+            var defaultColor = Console.ForegroundColor;
+
+            var colorList = new List<System.ConsoleColor>
+            {
+                ConsoleColor.White,
+                ConsoleColor.Yellow,
+                ConsoleColor.Cyan,
+                ConsoleColor.Green,
+                ConsoleColor.Red,
+                ConsoleColor.Magenta
+            };
+
+            // Change colors by character
+            var titleString = "CLEVERDICE!";
+            for (int i = 0; i < titleString.Length; i++)
+            {
+                Console.ForegroundColor = colorList.ElementAt(i % colorList.Count);
+                Console.Write(titleString.Substring(i, 1));
+            }
+
+            // Stop this madness
+            Console.ForegroundColor = defaultColor;
+        }
+    }
+}
